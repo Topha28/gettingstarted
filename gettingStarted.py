5a181bd9fd5bcbcb7c838b030b7c425d912e6573
Topha <sm13793@nyu.edu>
2026-01-30 18:13:10 -0500

    Submission of assignment 1

---
+++ gettingStarted.py
@@ -0,0 +1,33 @@
+def welcome_assignment_answers(question):
+
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 4
+
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "2a7cd1574b1a7cc82e3513bf1b9ebb46987746f94f3a3b3d522a011ae82da19b"
+
+    else:
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+
+    return answer
